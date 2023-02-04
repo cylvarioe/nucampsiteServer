90c27cb6d666238a3c4e449869302d4c5f48d74c
Jose Enriquez <cylvarioenriquez@gmail.com>
2023-02-03 18:50:23 -0900

    Express Session Part 2

--- app.js
+++ app.js
@@ -47,6 +47,9 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
@@ -83,10 +86,9 @@ function auth(req, res, next) {
 }
 
 app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
